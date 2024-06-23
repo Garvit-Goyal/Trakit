f5c9bc950c6472f9cbc1bbbcd3da33cbb90f9623
Garvit Goyal <garvitgoyal058@gmail.com>
2024-06-23 14:04:06 +0530

    First Commit (Project Setup)

---
+++ src/config/firebase-config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBcDYWOzbjd5Cos-Oqqhi_PXnH957g40CI",
+  authDomain: "trakit-2fb89.firebaseapp.com",
+  projectId: "trakit-2fb89",
+  storageBucket: "trakit-2fb89.appspot.com",
+  messagingSenderId: "319665084102",
+  appId: "1:319665084102:web:ff5b0f0cb351d2b64ae9c7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+//firebase login
+//firebase init
+//firebase deploy
