3a2c5fd8d92c7222bae8a1f34b21b6b175536331
Garvit Goyal <garvitgoyal058@gmail.com>
2024-07-02 02:55:23 +0530

    Auth Page CSS Start

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,33 +1,64 @@
-import {auth, provider} from "../../config/firebase-config"
-import {signInWithPopup} from "firebase/auth"
-import {useNavigate, Navigate} from "react-router-dom"
-import { useGetUserInfo } from "../../hooks/useGetUserInfo"
+import { auth, provider } from "../../config/firebase-config"
+import { signInWithPopup } from "firebase/auth"
+import { useNavigate, Navigate } from "react-router-dom"
+//import { useGetUserInfo } from "../../hooks/useGetUserInfo"
+
+import { FaUser } from "react-icons/fa"
+import { FaLock } from "react-icons/fa"
+import "./style.css"
+
+import logo from "../../assets/budget.png"
+
 
 export const Auth = () => {
 
     const navigate = useNavigate();
-    const {isAuth} = useGetUserInfo();
+    //const isAuth = useGetUserInfo();
 
-    const signInWithGoogle = async () =>{
+    const signInGoogle = async () => {
         const results = await signInWithPopup(auth, provider);
         //For keeping the user signed in even after refresh (local storage here but cookies is better).
         const authInfo = {
-            userID : results.user.uid,
-            name : results.user.displayName,
-            profilePhoto : results.user.photoURL,
-            isAuth : true,
+            userID: results.user.uid,
+            name: results.user.displayName,
+            profilePhoto: results.user.photoURL,
+            isAuth: true,
         };
         localStorage.setItem("auth", JSON.stringify(authInfo)) //Local storage takes only string not objects so converting to string.
         navigate("/expense-tracker");
     }
 
-    if (isAuth){
-        return <Navigate to="/expense-tracker"/>
-    }
+    // if (isAuth){
+    //     return <Navigate to="/expense-tracker"/>
+    // }
     return (
         <div className="login-page">
-            <p>Sign in with Google</p>
-            <button className="login-with-google-button" onClick={signInWithGoogle}>Sign in with Google</button>
+            <div className="login-card">
+                <form action="">
+                    <img src={logo} className="logo"/>
+                    <h1>Login to TrakIt</h1>
+                    <div className="input-box">
+                        <input type="text" placeholder="Username"></input>
+                        <FaUser className="icon" />
+                    </div>
+                    <div className="input-box">
+                        <input type="password" placeholder="Password"></input>
+                        <FaLock className="icon" />
+                    </div>
+                    <div className="forgot-password">
+                        <a href="">Forgot Password?</a>
+                    </div>
+
+                    <button>Log in</button>
+                    <div className="sign-up">
+                        <p>Don't have an account? <a href="">Register</a></p>
+                    </div>
+                </form>
+                <h2>-OR-</h2>
+                <div className="signInWithGoogle">
+                    <button className="login-with-google-button" onClick={signInGoogle}>Sign in with Google</button>
+                </div>
+            </div>
         </div>
     )
 }
