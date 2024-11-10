50ffbc607a0b3bf05575abe0c7446db4fc8e0ba7
Garvit Goyal <garvitgoyal058@gmail.com>
2024-11-10 16:48:42 +0530

    Google Auth - Email Removed

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -32,30 +32,13 @@ export const Auth = () => {
                 <form action="">
                     <img src={logo} className="logo"/>
                     <h1>Login to TrakIt</h1>
-                    <div className="input-box">
-                        <input type="text" placeholder="Username"></input>
-                        <FaUser className="icon" />
-                    </div>
-                    <div className="input-box">
-                        <input type="password" placeholder="Password"></input>
-                        <FaLock className="icon" />
-                    </div>
-                    <div className="forgot-password">
-                        <a href="">Forgot Password?</a>
-                    </div>
-
-                    <button className="log-in">Log in</button>
-                    <div className="sign-up">
-                        <p>Don't have an account? <a href="">Register</a></p>
-                    </div>
                 </form>
-                <h2>-OR-</h2>
                 <div className="signInWithGoogle">
                     <button className="login-with-google-button" onClick={signInGoogle}>&emsp; Sign in with Google</button>
                     <FcGoogle className="color-google-icon" />
                 </div>
                 <div className="github">
-                    <p><a href="https://github.com/Garvit-Goyal/Trakit">Demo Video</a> | <a href="https://github.com/Garvit-Goyal/Trakit">GitHub</a></p>
+                    <p><a href="https://github.com/Garvit-Goyal/Trakit">GitHub Repository</a></p>
                 </div>
             </div>
         </div>
